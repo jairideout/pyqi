41394e0d9cc85b3f03024050d02c20b36c4868b5
Jai Ram Rideout <jai.rideout@gmail.com>
2013-12-04 20:38:00 -0700

    Fix failing test (still testing out travis-ci)

--- tests/test_core/test_command.py
+++ tests/test_core/test_command.py
@@ -67,7 +67,6 @@ class CommandTests(TestCase):
         stub._validate_kwargs(kwargs)
         kwargs = {'a':10, 'b':20, 'c':20}
         self.assertRaises(ValueError, stub._validate_kwargs, kwargs)
-        self.assertEqual(42, 41)
 
     def test_set_defaults(self):
         stub = self.stubby()
